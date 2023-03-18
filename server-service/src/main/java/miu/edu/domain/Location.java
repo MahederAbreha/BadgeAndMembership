f591cd0001b582f37a8c20d58a81c36b3a992370
bijaymandal362 <bijaymandal362@gmail.com>
2023-03-18 18:48:38 -0500

    memeber class domain validation added

--- server-service/src/main/java/miu/edu/domain/Location.java
+++ server-service/src/main/java/miu/edu/domain/Location.java
@@ -1,4 +1,24 @@
 package miu.edu.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import miu.edu.domain.enums.LocationType;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Location {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+    private String description;
+    private String capacity;
+    private LocationType locationType;
 }
--- server-service/src/main/java/miu/edu/domain/Membership.java
+++ server-service/src/main/java/miu/edu/domain/Membership.java
@@ -1,4 +1,23 @@
 package miu.edu.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Membership {
+    @Id
+    @GeneratedValue
+    private long id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
 }
