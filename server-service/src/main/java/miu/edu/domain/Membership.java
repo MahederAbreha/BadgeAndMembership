17e9549bb69be0846909c22fab7e436da85a1a11
bijaymandal362 <bijaymandal362@gmail.com>
2023-03-18 18:51:52 -0500

    membership class is with date validation past and future annotation added

--- server-service/src/main/java/miu/edu/domain/Membership.java
+++ server-service/src/main/java/miu/edu/domain/Membership.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 @Entity
@@ -17,7 +19,11 @@ public class Membership {
     @Id
     @GeneratedValue
     private long id;
+
+    @Past(message = "Start date must be in the past")
     private LocalDate startDate;
+
+    @Future(message = "End date must be in the future")
     private LocalDate endDate;
 
 }
