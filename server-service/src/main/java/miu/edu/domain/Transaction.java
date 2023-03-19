12c05d8a553843183cd30ac79456e4b28851fb10
Santosh Manandhar <santosh.manandhar1@gmail.com>
2023-03-19 13:25:44 -0500

    syss

--- server-service/src/main/java/miu/edu/domain/Transaction.java
+++ server-service/src/main/java/miu/edu/domain/Transaction.java
@@ -9,6 +9,8 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -21,23 +23,21 @@ public class Transaction {
     @Column (name = "transaction_id")
     private long id;
 
+
     @Column (name = "transaction_status_type", nullable = false)
     @NotBlank(message = "Transaction_status_type is required")
-    private TransactionStatusType transactionStatusType; //Data Type ???? String or TransactionStatusType
+    private TransactionStatusType transactionStatusType;
 
     @Column (name = "transaction_date_time", nullable = false)
     @NotBlank(message = "Transaction_date_time is required")
     private LocalDateTime transactionDateTime;
 
-    //1-1 relationship with Membership
-    @ManyToOne
-    @JoinColumn(name="membership_id")
-    private Membership membership;
+    @ManyToOne()
+    @JoinColumn(name = "membership_id")
+    private Membership memberships;
 
-    //1-1 relationship with Location
-    @ManyToOne
-    @JoinColumn(name="location_id")
+    @ManyToOne()
+    @JoinColumn(name = "location_id")
     private Location location;
 
-    
 }
