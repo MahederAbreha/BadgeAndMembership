c0e95da6c2779afbb44a57575ce5a937a4597f33
Santosh Manandhar <santosh.manandhar1@gmail.com>
2023-03-19 12:39:03 -0500

    transaction CRUD

--- server-service/src/main/java/miu/edu/controller/TransactionController.java
+++ server-service/src/main/java/miu/edu/controller/TransactionController.java
@@ -1,4 +1,47 @@
 package miu.edu.controller;
 
+import lombok.RequiredArgsConstructor;
+import miu.edu.dto.TransactionDTO;
+import miu.edu.service.TransactionService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
+@RequiredArgsConstructor
 public class TransactionController {
+    @Autowired
+    private final TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<?> addTransaction(TransactionDTO transactionDTO){
+        transactionService.addTransaction(transactionDTO);
+        return new ResponseEntity<String>("Transaction saved.", HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<?> getAllTransactions(){
+        return new ResponseEntity<List<TransactionDTO>>(transactionService.findAllTransaction(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{member_id}")
+    public ResponseEntity<?> getTransaction(@PathVariable Long transaction_id){
+        return new ResponseEntity<TransactionDTO>(transactionService.findById(transaction_id), HttpStatus.OK);
+    }
+    @PutMapping
+    public ResponseEntity<?> updateTransaction(TransactionDTO transactionDTO){
+        return new ResponseEntity<TransactionDTO>(transactionService.updateTransaction(transactionDTO), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{transaction_id}")
+    public ResponseEntity<?> deleteTransaction(@PathVariable Long transaction_id){
+        return new ResponseEntity<String>(transactionService.deleteById(transaction_id), HttpStatus.OK);
+    }
+
+
+
 }
--- server-service/src/main/java/miu/edu/domain/Transaction.java
+++ server-service/src/main/java/miu/edu/domain/Transaction.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import miu.edu.domain.enums.TransactionStatusType;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -24,12 +21,32 @@ public class Transaction {
     @Column (name = "transaction_id")
     private long id;
 
-
     @Column (name = "transaction_status_type", nullable = false)
     @NotBlank(message = "Transaction_status_type is required")
-    private TransactionStatusType transactionStatusType;
+    private TransactionStatusType transactionStatusType; //Data Type ???? String or TransactionStatusType
 
     @Column (name = "transaction_date_time", nullable = false)
     @NotBlank(message = "Transaction_date_time is required")
     private LocalDateTime transactionDateTime;
+
+    //1-1 relationship with Membership
+    @OneToOne
+    @JoinColumn(name="membership_id")
+    private Membership membership;
+
+    //1-1 relationship with Location
+    @OneToOne
+    @JoinColumn(name="location_id")
+    private Location location;
+
+    @Embedded
+    private Audit audit;
+
+    public Transaction(long id, TransactionStatusType transactionStatusType, LocalDateTime transactionDateTime, Membership membership, Location location) {
+        this.id = id;
+        this.transactionStatusType = transactionStatusType;
+        this.transactionDateTime = transactionDateTime;
+        this.membership = membership;
+        this.location = location;
+    }
 }
