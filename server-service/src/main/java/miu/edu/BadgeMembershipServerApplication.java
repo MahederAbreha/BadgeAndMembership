20c7e256f728cd187eb74cff05d2c61d14cde318
Santosh Manandhar <santosh.manandhar1@gmail.com>
2023-03-20 13:25:13 -0500

    update in transaction

--- server-service/src/main/java/miu/edu/BadgeMembershipServerApplication.java
+++ server-service/src/main/java/miu/edu/BadgeMembershipServerApplication.java
@@ -1,13 +1,19 @@
 package miu.edu;
 
+import miu.edu.domain.Transaction;
 import miu.edu.repository.MemberRepository;
+import miu.edu.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class BadgeMembershipServerApplication {
+public class BadgeMembershipServerApplication{
 
+    @Autowired
+    TransactionRepository transactionRepository;
     public static void main(String[] args) {
         SpringApplication.run(BadgeMembershipServerApplication.class, args);
         System.out.println("Server is running!!!!!!");
--- server-service/src/main/java/miu/edu/repository/MemberRepository.java
+++ server-service/src/main/java/miu/edu/repository/MemberRepository.java
@@ -1,9 +1,18 @@
 package miu.edu.repository;
 
 import miu.edu.domain.Member;
+import miu.edu.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    //return the list of all transactions (GET /members/{memberId}/transactions)
+//    @Query("SELECT m FROM MEMBER m JOIN m.membership ms JOIN ms.TRANSACTION t WHERE t.member_id =: member_id");
+//    List<Transaction> getTransactionByMemberId(@Param("member_id") long member_id);
+
 }
